f7ce7bbe540f877c976469de4772da69fe4d38bf
Arne Wiese <arne.wiese@gmail.com>
2019-01-30 10:22:35 +0100

    Removed ui from css

--- ui/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-export declare type Props = {
-    disabled: boolean;
-};
-declare const Button: React.FunctionComponent<Props>;
-export default Button;
--- ui/components/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-const Button = ({ children, ...props }) => (React.createElement("button", Object.assign({}, props), children));
-Button.defaultProps = {
-    disabled: false
-};
-export default Button;
